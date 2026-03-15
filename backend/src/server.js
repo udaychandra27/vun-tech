ab3259851139ce251e84f989cc8ab61e5a67e36e
vaanis11 <vaanis1110@gmail.com>
2026-03-15 17:40:25 +0530

    cloudinary 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,8 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.set("trust proxy", 1)
+
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
